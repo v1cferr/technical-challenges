c8960217132b2b3a46f2c8a15012f22f98f4a9e5
v1cferr <dev.victorferreira@gmail.com>
2025-03-30 15:01:42 -0300

    chore(eslint): changing rules to default by next and install eslint plugin deps

--- supersign/eslint.config.mjs
+++ supersign/eslint.config.mjs
@@ -1,36 +1,16 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default defineConfig([
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        ...globals.browser,
-        React: true,
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-    },
-    settings: {
-      react: {
-        version: "19.1.0",
-      },
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/no-explicit-any": "warn",
-    },
-  },
-]);
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["eslint:recommended", "next"],
+  }),
+];
+
+export default eslintConfig;
